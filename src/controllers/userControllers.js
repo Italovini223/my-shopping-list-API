03322c8055a88ca793a0eed6ae04d880d0dc4942
italo <italovinicius2018@gmail.com>
2022-08-29 03:16:22 -0300

    🐛 fix: fix a bug in userController line 8

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -1,22 +1,26 @@
 const knex = require("../database/knex");
 const appError = require("../utils/appError")
 const {hash} = require("bcryptjs");
+const sqliteConnection = require("../database/sqlite")
+
 class UserControllers {
  async create(request, response){
     const {name, email, password} = request.body;
-    
-    const checkUserExists = await knex("users").where({email});
+    const database = await sqliteConnection();
+
+    const checkUserExists = await database.get("SELECT * FROM users WHERE email = (?)", [email])
+
     if(checkUserExists){
-      throw new appError("User already exists")
-    } else {
-      const hashedPassword = await hash(password, 8)
+      throw new appError("User already exists");
+    } 
+    
+    const hashedPassword = await hash(password, 8)
       
-      await knex("users").insert({
-        name,
-        email,
-        password: hashedPassword
-      });
-    }
+    await knex("users").insert({
+      name,
+      email,
+      password: hashedPassword
+    });
 
     return response.status(200).json({
       message: "User registered successfully"
