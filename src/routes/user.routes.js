067652dd963fcbb5284cef3b5e6167429b9f45f5
italo <italovinicius2018@gmail.com>
2022-08-28 21:14:00 -0300

    ✨feat: add a rote to create user account

---
+++ src/routes/user.routes.js
@@ -0,0 +1,11 @@
+const Router = require("express");
+
+const {UserControllers} = require("../controllers/userControllers");
+
+const userRoutes = Router();
+
+const userControllers = new UserControllers();
+
+userRoutes.post("/", userControllers.create);
+
+module.exports = userRoutes;
