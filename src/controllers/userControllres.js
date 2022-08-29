39e97eaad513f3ad3b178f03654628febeddf10f
italo <italovinicius2018@gmail.com>
2022-08-28 21:06:05 -0300

    ✅ test: add a test in userContollers.create

--- src/controllers/userControllres.js
+++ src/controllers/userControllres.js
@@ -1,5 +1,13 @@
 export class UserControllers {
-
+  create(request, response){
+    const {name, email, password} = request.body;
+    
+    return response.json({
+      name,
+      email,
+      password
+    });
+  }
 };
 
 module.exports = UserControllers;
