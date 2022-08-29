73179d21a8c19ffaea676923f64cd49ea3d3c4cd
italo <italovinicius2018@gmail.com>
2022-08-29 02:02:20 -0300

    🔒️ feat: add encryption on password

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -1,18 +1,20 @@
 const knex = require("../database/knex");
 const appError = require("../utils/appError")
+const {hash} = require("bcryptjs");
 class UserControllers {
  async create(request, response){
     const {name, email, password} = request.body;
     
     const checkUserExists = await knex("users").where({email});
-
     if(checkUserExists){
       throw new appError("User already exists")
     } else {
+      const hashedPassword = await hash(password, 8)
+      
       await knex("users").insert({
         name,
         email,
-        password
+        password: hashedPassword
       });
     }
 
