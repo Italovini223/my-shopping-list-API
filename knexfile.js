f723dda8e15e272c62f0459dbf6e08d0397d092b
italo <italovinicius2018@gmail.com>
2022-08-28 23:08:28 -0300

    ✨ feat: config the knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -1,47 +1,15 @@
-// Update with your config settings.
-
-/**
- * @type { Object.<string, import("knex").Knex.Config> }
- */
+const path = require("path");
 module.exports = {
 
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './dev.sqlite3'
+      filename: path.resolve(--dirname, "src", "database", "database.db")
+    },
+    migrations:{
+      directory: path.resolve(__dirname, "src", "database", "knex", "migrations")
     }
   },
 
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
-
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  }
 
 };
