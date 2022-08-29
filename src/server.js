69b9ad71aa2b92d10b3d40dc7fdca4c82bf898c9
italo <italovinicius2018@gmail.com>
2022-08-28 22:07:53 -0300

    ✨feat: add a error handling

--- src/server.js
+++ src/server.js
@@ -5,9 +5,25 @@ const routes = require("./routes");
 
 const app = express();
 
-const PORT = 3333;
 
 app.use(express.json())
 app.use(routes)
 
+app.use((error, request, response, next) => {
+  if(error instanceof AppError){
+    return response.status(error.statusCode).json({
+      status: "Invalid entry",
+      message: error.message
+    });
+  }
+  
+  console.log(error.message);
+  
+  return response.status(500).json({
+    status: "Server Error",
+    message: "Internal error"
+  });
+});
+
+const PORT = 3333;
 app.listen(PORT, () => `server is running on port ${PORT}`);
