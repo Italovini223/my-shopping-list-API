be6a0c41629b7f08931b9b92f72322793803f8df
italo <italovinicius2018@gmail.com>
2022-09-12 11:32:26 -0300

    ✨ feat: add a route to delete user

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -8,5 +8,6 @@ const userControllers = new UserControllers();
 
 userRoutes.post("/", userControllers.create);
 userRoutes.put("/:id", userControllers.update);
+userRoutes.delete("/:id", userControllers.delete);
 
 module.exports = userRoutes;
