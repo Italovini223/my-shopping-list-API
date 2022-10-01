966f67eb01eca211fffc186082e4a84f1d6b1e1b
Italovini223 <italovinicius2018@gmail.com>
2022-10-01 16:39:02 -0300

    ✨ feat: add route to index list

--- src/routes/list.routes.js
+++ src/routes/list.routes.js
@@ -8,5 +8,6 @@ const listControllers = new  ListControllers();
 
 listRoutes.post("/:user_id", listControllers.create);
 listRoutes.get("/:id", listControllers.show);
+listRoutes.get("/:user_id", listControllers.index);
 
 module.exports = listRoutes;
