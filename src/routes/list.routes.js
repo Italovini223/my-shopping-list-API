7a9030c419d4f2963060fae87a257a7b7bd66a4b
Italovini223 <italovinicius2018@gmail.com>
2022-10-01 16:19:54 -0300

    ✨ feat: add route to show list

--- src/routes/list.routes.js
+++ src/routes/list.routes.js
@@ -7,5 +7,6 @@ const listRoutes = Router();
 const listControllers = new  ListControllers();
 
 listRoutes.post("/:user_id", listControllers.create);
+listRoutes.get("/:id", listControllers.show);
 
 module.exports = listRoutes;
