451985e5dc8cc1312155967fe3302580f8727749
Italovini223 <italovinicius2018@gmail.com>
2022-10-01 16:07:10 -0300

    🐛 fix; bug primary key on crate list

--- src/controllers/listControllers.js
+++ src/controllers/listControllers.js
@@ -6,6 +6,24 @@ class ListControllers {
     const {user_id} = request.params;
     const {title, description, products} = request.body;
 
+    const list_id = await knex("list").insert({
+    title,
+    description,
+    user_id: Number(user_id)
+    });
+
+    const productsInsert = products.map(product => {
+      return {
+        user_id,
+        list_id,
+        name: product
+      }
+    });
+
+    await knex("products").insert(productsInsert);
+
+    return response.status(201).json("lista criada com sucesso");
+
 
   }
 }
--- src/database/knex/migrations/20221001164419_createProducts.js
+++ src/database/knex/migrations/20221001164419_createProducts.js
@@ -2,7 +2,7 @@
 exports.up = knex => knex.schema.createTable("products", table => {
   table.increments("id");
 
-  table.integer("user_id").references("id").inTable("users").onDelete("CASCADE");
+  table.integer("user_id").references("id").inTable("users");
   table.integer("list_id").references("id").inTable("list").onDelete("CASCADE");
 
   table.text("name");
