b357522b5253741e41413275e6f3958ec9543592
Italovini223 <italovinicius2018@gmail.com>
2022-10-01 13:55:32 -0300

    ✨feat:  add products table in database

--- src/database/knex/migrations/20221001164419_createProducts.js
+++ src/database/knex/migrations/20221001164419_createProducts.js
@@ -1,15 +1,12 @@
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
-exports.up = function(knex) {
-  
-};
 
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
-exports.down = function(knex) {
-  
-};
+exports.up = knex => knex.schema.createTable("products", table => {
+  table.increments("id");
+
+  table.integer("user_id").references("id").inTable("users").onDelete("CASCADE");
+  table.integer("list_id").references("id").inTable("list").onDelete("CASCADE");
+
+  table.text("name");
+
+})
+
+exports.down = knex => knex.schema.dropTable("products");
