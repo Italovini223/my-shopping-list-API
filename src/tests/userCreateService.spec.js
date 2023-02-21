59a73aa0ffc1cd2da35295b89ec026ecf2e97074
Italovini223 <italovinicius2018@gmail.com>
2023-02-20 21:50:30 -0300

    ✅ test: add beforeEach

--- src/tests/userCreateService.spec.js
+++ src/tests/userCreateService.spec.js
@@ -3,6 +3,16 @@ const UserRepositoryInMemory  = require('../repositories/UserRepositoryInMemory'
 const appError = require('../utils/appError');
 
 describe("user create service", () => {
+  let userRepositoryInMemory;
+  let userCreateService;
+
+  beforeEach(() => {
+    userRepositoryInMemory = new UserRepositoryInMemory();
+    userCreateService = new UserCreateService(userRepositoryInMemory);
+  })
+
+
+
   it("should be able to create a new user", async () => {
     const user = {
       name: "teste",
@@ -10,8 +20,7 @@ describe("user create service", () => {
       password: "123"
     }
   
-    const userRepositoryInMemory = new UserRepositoryInMemory();
-    const userCreateService = new UserCreateService(userRepositoryInMemory);
+  
     const userCrated = await userCreateService.execute(user);
   
     expect(userCrated).toHaveProperty("id")
@@ -30,11 +39,11 @@ describe("user create service", () => {
       password: "test123"
     }
 
-    const userRepositoryInMemory = new UserRepositoryInMemory();
-    const userCreateService = new UserCreateService(userRepositoryInMemory);
-
     await userCreateService.execute(user1);
-    await expect(userCreateService.execute(user2)).rejects.toEqual(new appError("User already exists"))
+
+    expect(async () => {
+      await userCreateService.execute(user2)
+    }).rejects.toEqual(new appError("User already exists"))
   })
 })
 
